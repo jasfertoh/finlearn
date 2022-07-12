ee3b2842f204098e115ed689aafa96deafa01c03
Jasfer <59435921+jasfertoh@users.noreply.github.com>
2022-07-12 14:48:35 +0800

    made changes
    - added responsive support for navbar
    - added responsive changes

---
+++ script.js
@@ -0,0 +1,22 @@
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("hamburger");
+const bgOverlay = document.querySelector(".overlay");
+
+if (burgerMenu && bgOverlay) {
+  burgerMenu.addEventListener("click", () => {
+    navbarMenu.classList.add("is-active");
+    bgOverlay.classList.toggle("is-active");
+  });
+
+  bgOverlay.addEventListener("click", () => {
+    navbarMenu.classList.remove("is-active");
+    bgOverlay.classList.toggle("is-active");
+  });
+}
+
+document.querySelectorAll(".menu-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarMenu.classList.remove("is-active");
+    bgOverlay.classList.remove("is-active");
+  });
+});
