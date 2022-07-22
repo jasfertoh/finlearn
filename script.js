7966728064801ea7c27dcb5aef7aa265d01e7bbe
Jasfer Toh <jasfertoh@gmail.com>
2022-07-22 15:40:23 +0800

    added course details on click

--- script.js
+++ script.js
@@ -39,3 +39,43 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+// Give the parameter a variable name
+var dynamicContent = getParameterByName("dc");
+
+$(document).ready(function () {
+  // Check if the URL parameter is apples
+  if (dynamicContent == "apples") {
+    $("#apples").show();
+  }
+  // Check if the URL parameter is oranges
+  else if (dynamicContent == "oranges") {
+    $("#oranges").show();
+  }
+  // Check if the URL parameter is bananas
+  else if (dynamicContent == "bananas") {
+    $("#bananas").show();
+  }
+  // Check if the URL parmeter is empty or not defined, display default content
+  else {
+    $("#default-content").show();
+  }
+});
+
+var course = document.getElementsByClassName("course");
+
+for (let k = 0; k < course.length; k++) {
+  course[k].addEventListener("click", function () {
+    var courseID = course[k].id;
+    window.location.href = `/courses.html?course=${courseID}`;
+  });
+}
